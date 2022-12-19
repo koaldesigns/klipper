3d0a963d00aec219c839af3d864d6ba4207ccfc0
koaldesigns <koaldesigns@gmail.com>
2022-12-19 11:29:51 -0600

    Added scara to init

--- klippy/chelper/__init__.py
+++ klippy/chelper/__init__.py
@@ -21,7 +21,7 @@ SOURCE_FILES = [
     'pollreactor.c', 'msgblock.c', 'trdispatch.c',
     'kin_cartesian.c', 'kin_corexy.c', 'kin_corexz.c', 'kin_delta.c',
     'kin_deltesian.c', 'kin_polar.c', 'kin_rotary_delta.c', 'kin_winch.c',
-    'kin_extruder.c', 'kin_shaper.c',
+    'kin_extruder.c', 'kin_shaper.c', 'kin_scara.c',
 ]
 DEST_LIB = "c_helper.so"
 OTHER_FILES = [
@@ -97,6 +97,11 @@ defs_trapq = """
         , double pos_x, double pos_y, double pos_z);
     int trapq_extract_old(struct trapq *tq, struct pull_move *p, int max
         , double start_time, double end_time);
+"""
+defs_kin_scara = """
+    struct stepper_kinematics *scara_stepper_alloc(char arm,
+                    double proximal_length, double distal_length,
+                    double crosstalk, double arm_mode);
 """
 
 defs_kin_cartesian = """
@@ -211,7 +216,7 @@ defs_all = [
     defs_itersolve, defs_trapq, defs_trdispatch,
     defs_kin_cartesian, defs_kin_corexy, defs_kin_corexz, defs_kin_delta,
     defs_kin_deltesian, defs_kin_polar, defs_kin_rotary_delta, defs_kin_winch,
-    defs_kin_extruder, defs_kin_shaper,
+    defs_kin_extruder, defs_kin_shaper, defs_kin_scara,
 ]
 
 # Update filenames to an absolute path
